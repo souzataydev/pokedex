53d2e2e95af40df11b98abedf19cc36e4b13291d
Taynara Souza <souzacampostay@outlook.com>
2023-09-13 18:57:23 -0300

    Adicionando os tipos de Pokémon dinamicamente

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,12 @@
 function convertPokemonToLi(pokemon){
     return `
-    <li class="pokemon">
-    <span class="number">#0${pokemon.order}</span>
+    <li class="pokemon ${pokemon.type}">
+    <span class="number">#${pokemon.order}</span>
     <span class="name">${pokemon.name}</span>
 
     <div class="detail">
         <ol class="types">
-        ${pokemon.types.map((type) => `<li class="type"> ${type} </li>`).join('')}
+        ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
         </ol>
 
         <img src="${pokemon.photo}"
